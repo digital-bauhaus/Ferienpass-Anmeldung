b12b467d5ba06622c4a768546bc733416788a423
Philipp Rudloff <philipp.rudloff@uni-weimar.de>
2017-12-05 13:14:50 +0100

    toIdentifier: Handle ä (ae), ü (ue), ö (oe), ß (ss)

--- frontend/src/mixins/to-identifier.js
+++ frontend/src/mixins/to-identifier.js
@@ -3,6 +3,10 @@ export const toIdentifier = {
     toIdentifier: function(value) {
       return value
         .toLowerCase()
+        .replace('ä', 'ae')
+        .replace('ö', 'oe')
+        .replace('ü', 'ue')
+        .replace('ß', 'ss')
         .replace(/[^\w ]+/g, '')
         .replace(/ +/g, '-');
     }
