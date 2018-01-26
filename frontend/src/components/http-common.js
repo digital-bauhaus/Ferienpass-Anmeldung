ffb0171a136aab62a3c6b60c87013b307d8e0040
Philipp Rudloff <philipp.rudloff@uni-weimar.de>
2018-01-26 10:51:11 +0100

    Change port from 808x to 809x

--- frontend/src/components/http-common.js
+++ frontend/src/components/http-common.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const AXIOS = axios.create({
-  baseURL: `http://localhost:8088`,
+  baseURL: `http://localhost:8098`,
   headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:8080'
+    'Access-Control-Allow-Origin': 'http://localhost:8090'
   }
 });
