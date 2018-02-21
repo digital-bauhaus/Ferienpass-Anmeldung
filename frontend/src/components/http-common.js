6c7bd4ba70503acf5d3aee0d0fbaa66bd35ebc31
Philipp Rudloff <philipp.rudloff@uni-weimar.de>
2018-02-21 17:45:24 +0100

    Talk to admin backend

--- frontend/src/components/http-common.js
+++ frontend/src/components/http-common.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const AXIOS = axios.create({
-  baseURL: `http://localhost:8098`,
+  baseURL: 'http://localhost:8098',
   headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:8090'
+    'Access-Control-Allow-Origin': 'http://localhost:8088'
   }
 });
