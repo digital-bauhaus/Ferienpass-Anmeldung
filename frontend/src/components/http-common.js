ed0f3001ee5bac7559e92aec640d6dc7fba03173
Jonas Hecht <jonas.hecht@codecentric.de>
2018-04-10 21:29:20 +0200

    #36: Correcting CORS-config

--- frontend/src/components/http-common.js
+++ frontend/src/components/http-common.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const AXIOS = axios.create({
   baseURL: '/api',
   headers: {
-    'Access-Control-Allow-Origin': 'https://ferienpass-admin.herokuapp.com'
+    'Access-Control-Allow-Origin': 'https://ferienpass-anmeldung.herokuapp.com/'
   }
 });
