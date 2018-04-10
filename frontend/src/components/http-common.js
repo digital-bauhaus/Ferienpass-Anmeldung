653247322048ef0f8e9416dd088eefa626b56c5e
Jonas Hecht <jonas.hecht@codecentric.de>
2018-04-10 21:11:41 +0200

    #36: Configuring CORS again for accessing Admin-Service on Heroku from Heroku :)

--- frontend/src/components/http-common.js
+++ frontend/src/components/http-common.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export const AXIOS = axios.create({
-  baseURL: '/api'
+  baseURL: '/api',
+  headers: {
+    'Access-Control-Allow-Origin': 'https://ferienpass-admin.herokuapp.com'
+  }
 });
